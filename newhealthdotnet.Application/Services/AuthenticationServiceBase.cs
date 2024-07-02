a44601b0abc127dd1eebabaa3ce34f6651f9dd51
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-02 08:59:15 +0300

    solved an error

--- newhealthdotnet.Application/Services/AuthenticationServiceBase.cs
+++ newhealthdotnet.Application/Services/AuthenticationServiceBase.cs
@@ -6,18 +6,5 @@ namespace newhealthdotnet.Application.Services
 {
     public class AuthenticationServiceBase : AuthenticationServiceBaseBase
     {
-
-        public async Task<AuthenticationResponse> LoginAsync(LoginRequest request)
-        {
-            var user = await _userRepository.GetUserByEmailAsync(request.Email);
-            if (!(user != null && BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash)))
-            {
-                throw new Exception("Invalid credentials");
-            }
-
-            // var token = _jwtTokenGenerator.GenerateToken(user);
-
-            //  return new AuthenticationResponse { Token = token };
-        }
     }
 }
--- newhealthdotnet.Application/Services/AuthenticationServiceBaseBase.cs
+++ newhealthdotnet.Application/Services/AuthenticationServiceBaseBase.cs
@@ -6,6 +6,19 @@ namespace newhealthdotnet.Application.Services
 {
     public class AuthenticationServiceBaseBase
     {
+
+        public async Task<AuthenticationResponse> LoginAsync(LoginRequest request)
+        {
+            var user = await _userRepository.GetUserByEmailAsync(request.Email);
+            if (!(user != null && BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash)))
+            {
+                throw new Exception("Invalid credentials");
+            }
+
+            // var token = _jwtTokenGenerator.GenerateToken(user);
+
+            //  return new AuthenticationResponse { Token = token };
+        }
         // private readonly ITokenGenerator _tokenGenerator = tokenGenerator;
 
         public async Task<AuthenticationResponse> RegisterAsync(RegisterRequest request)
