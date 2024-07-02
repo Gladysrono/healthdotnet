1e0581e864f9c34f0a6b87226dbc8d399f7f2c65
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-02 08:48:18 +0300

    solved an error

--- newhealthdotnet.Application/Services/AuthenticationService.cs
+++ newhealthdotnet.Application/Services/AuthenticationService.cs
@@ -20,33 +20,6 @@ namespace newhealthdotnet.Application.Services
         //public IEmailSender EmailSender { get; } = emailSender;
 
         private readonly IEmailSender _emailSender = emailSender;
-        // private readonly ITokenGenerator _tokenGenerator = tokenGenerator;
-
-        public async Task<AuthenticationResponse> RegisterAsync(RegisterRequest request)
-        {
-            var existingUser = await _userRepository.GetUserByEmailAsync(request.Email);
-            if (existingUser != null)
-            {
-                throw new Exception("User already exists");
-            }
-
-            var user = new User
-            {
-                Id = Guid.NewGuid(),
-                FirstName = request.FirstName,
-                LastName = request.LastName,
-                Email = request.Email,
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
-                ResetPasswordToken = string.Empty, // Initialize required member
-                ResetPasswordTokenExpiry = null
-            };
-
-            await _userRepository.AddUserAsync(user);
-
-            //var token = _jwtTokenGenerator.GenerateToken(user);
-
-            //return new AuthenticationResponse { Token = token };
-        }
 
         public async Task ForgetPasswordAsync(ForgetPassword request)
         {
--- newhealthdotnet.Application/Services/AuthenticationServiceBase.cs
+++ newhealthdotnet.Application/Services/AuthenticationServiceBase.cs
@@ -1,4 +1,5 @@
 ﻿using newhealthdiotnet.Contracts.Authentication;
+using newhealthdotnet.Domain.Entities.UserManagement;
 
 namespace newhealthdotnet.Application.Services
 {
@@ -16,6 +17,33 @@ namespace newhealthdotnet.Application.Services
             // var token = _jwtTokenGenerator.GenerateToken(user);
 
             //  return new AuthenticationResponse { Token = token };
+        }
+        // private readonly ITokenGenerator _tokenGenerator = tokenGenerator;
+
+        public async Task<AuthenticationResponse> RegisterAsync(RegisterRequest request)
+        {
+            var existingUser = await _userRepository.GetUserByEmailAsync(request.Email);
+            if (existingUser != null)
+            {
+                throw new Exception("User already exists");
+            }
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
+                ResetPasswordToken = string.Empty, // Initialize required member
+                ResetPasswordTokenExpiry = null
+            };
+
+            await _userRepository.AddUserAsync(user);
+
+            //var token = _jwtTokenGenerator.GenerateToken(user);
+
+            //return new AuthenticationResponse { Token = token };
         }
     }
 }
