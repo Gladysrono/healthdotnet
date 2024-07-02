6212383861004d3520dd92ca0587ed0db6ede4b0
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-02 08:58:39 +0300

    solved many errors

--- newhealthdotnet.Application/Services/AuthenticationServiceBase.cs
+++ newhealthdotnet.Application/Services/AuthenticationServiceBase.cs
@@ -4,7 +4,7 @@ using newhealthdotnet.Infrastructure.Repositories;
 
 namespace newhealthdotnet.Application.Services
 {
-    public class AuthenticationServiceBase
+    public class AuthenticationServiceBase : AuthenticationServiceBaseBase
     {
 
         public async Task<AuthenticationResponse> LoginAsync(LoginRequest request)
@@ -18,33 +18,6 @@ namespace newhealthdotnet.Application.Services
             // var token = _jwtTokenGenerator.GenerateToken(user);
 
             //  return new AuthenticationResponse { Token = token };
-        }
-        // private readonly ITokenGenerator _tokenGenerator = tokenGenerator;
-
-        public async Task<AuthenticationResponse> RegisterAsync(RegisterRequest request)
-        {
-            var existingUser = await _userRepository.GetUserByEmailAsync(request.Email);
-            if (existingUser != null)
-            {
-                throw new Exception("User already exists");
-            }
-
-            var user = new User
-            {
-                Id = Guid.NewGuid(),
-                FirstName = request.FirstName,
-                LastName = request.LastName,
-                Email = request.Email,
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
-                ResetPasswordToken = string.Empty, // Initialize required member
-                ResetPasswordTokenExpiry = null
-            };
-
-            await IUserRepository.AddUserAsync(user);
-
-            //var token = _jwtTokenGenerator.GenerateToken(user);
-
-            //return new AuthenticationResponse { Token = token };
         }
     }
 }
---
+++ newhealthdotnet.Application/Services/AuthenticationServiceBaseBase.cs
@@ -0,0 +1,37 @@
+﻿using newhealthdiotnet.Contracts.Authentication;
+using newhealthdotnet.Domain.Entities.UserManagement;
+using newhealthdotnet.Infrastructure.Repositories;
+
+namespace newhealthdotnet.Application.Services
+{
+    public class AuthenticationServiceBaseBase
+    {
+        // private readonly ITokenGenerator _tokenGenerator = tokenGenerator;
+
+        public async Task<AuthenticationResponse> RegisterAsync(RegisterRequest request)
+        {
+            var existingUser = await _userRepository.GetUserByEmailAsync(request.Email);
+            if (existingUser != null)
+            {
+                throw new Exception("User already exists");
+            }
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
+                ResetPasswordToken = string.Empty, // Initialize required member
+                ResetPasswordTokenExpiry = null
+            };
+
+            await IUserRepository.AddUserAsync(user);
+
+            //var token = _jwtTokenGenerator.GenerateToken(user);
+
+            //return new AuthenticationResponse { Token = token };
+        }
+    }
+}
